8023b89b7878934af623690bb0e7514fa0e0bbbc
Richard Steen <rsteen@covermymeds.com>
2020-05-06 13:11:20 -0500

    lesson5

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,3 +1,8 @@
 <template>
-  <h1>Events Listing</h1>
+  <div>
+    <h1>Events Listing</h1>
+    <p>
+      <router-link :to="{ name: 'event-show', params: { id: '1' } }">First Event</router-link>
+    </p>
+  </div>
 </template>
