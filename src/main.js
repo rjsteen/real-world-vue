20669efaccd3c9e61060bedf429aae21d7a3a821
Richard Steen <rsteen@covermymeds.com>
2020-05-12 09:59:59 -0500

    finished this course

--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
 import Vue from "vue";
+import upperFirst from "lodash/upperFirst";
+import camelCase from "lodash/camelCase";
+
+const requireComponent = require.context(
+  "./components",
+  false,
+  /Base[A-Z]\w+\.(vue|js)$/
+);
+
+requireComponent.keys().forEach((fileName) => {
+  const componentConfig = requireComponent(fileName);
+
+  const componentName = upperFirst(
+    camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, "$1"))
+  );
+
+  Vue.component(componentName, componentConfig.default || componentConfig);
+});
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
@@ -8,5 +27,5 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
