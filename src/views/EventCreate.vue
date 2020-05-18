9308a99210bd1189d97c89d90ad1f1bd9597fc31
Richard Steen <rsteen@covermymeds.com>
2020-05-18 14:35:37 -0500

    commit a thing

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event is created by {{ user.id }}</p>
+    <p>There are {{ catLength }} categories</p>
+    <p>{{ getEventById(2) }}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters(["getEventById"]),
+    ...mapState(["user", "categories"]),
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
